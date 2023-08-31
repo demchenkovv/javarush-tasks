f0a3ecbfd50ef0c18f0d3fd764e3a73193ff1546
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-08-31 19:19:37 +0300

    level13.lecture01.task1: Iterator

---
+++ src/main/java/com/github/demchenkovv/level13/lecture01/task1/Solution.java
@@ -0,0 +1,38 @@
+package com.github.demchenkovv.level13.lecture01.task1;
+
+/*
+Выводим в консоли элементы множества
+*/
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Solution {
+
+    public static void print(HashSet<String> words) {
+        //напишите тут ваш код
+        Iterator<String> iterator = words.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static void main(String[] args) {
+        HashSet<String> words = new HashSet<>(
+                Arrays.asList("Программированию обычно учат на примерах."
+                        .split(" ")));
+        print(words);
+    }
+}
+
+/*
+В классе Solution есть метод print(HashSet<String>),
+который должен выводить в консоли все элементы множества, используя iterator().
+Метод main не участвует в проверке.
+
+Требования:
+•	В классе Solution должен быть публичный статический метод print(HashSet<String>)
+с типом возвращаемого значения void.
+•	Метод print(HashSet<String>) должен работать согласно условию.
+ */
