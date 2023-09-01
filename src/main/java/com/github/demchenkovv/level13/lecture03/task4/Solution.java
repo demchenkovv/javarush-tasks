4cfd474147d9aca6a4563251fbb906593df9fa30
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-01 21:41:52 +0300

    level13.lecture03.task4: HashMap / Копирование из ArrayList в HashMap, где ключ - индекс списка

---
+++ src/main/java/com/github/demchenkovv/level13/lecture03/task4/Solution.java
@@ -0,0 +1,46 @@
+package com.github.demchenkovv.level13.lecture03.task4;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ArrayList vs HashMap
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(getProgrammingLanguages());
+    }
+
+    public static HashMap<Integer, String> getProgrammingLanguages() {
+        //напишите тут ваш код
+        ArrayList<String> programmingLanguages = new ArrayList<>();
+
+        programmingLanguages.add("Java");
+        programmingLanguages.add("Kotlin");
+        programmingLanguages.add("Go");
+        programmingLanguages.add("Javascript");
+        programmingLanguages.add("Typescript");
+        programmingLanguages.add("Python");
+        programmingLanguages.add("PHP");
+        programmingLanguages.add("C++");
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        for (String s: programmingLanguages) {
+            hashMap.put(programmingLanguages.indexOf(s), s);
+        }
+
+        return hashMap;
+    }
+}
+
+/*
+В классе Solution есть метод getProgrammingLanguages, который возвращает список языков программирования.
+Тебе нужно переписать этот метод, чтобы он возвращал HashMap<Integer, String>.
+Ключом в этой коллекции будет индекс элемента в ArrayList.
+
+Требования:
+•	В классе Solution должен быть публичный статический метод getProgrammingLanguages с типом возвращаемого значения HashMap<Integer, String>.
+•	Метод getProgrammingLanguages() должен возвращать HashMap заполненным в соответствии с списком.
+ */
