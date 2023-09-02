dd1f5ccdfeed5845e72ada699f11430985c1954d
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-02 15:33:04 +0300

    level13.lecture05.task1: Enum / Создать Enum и вывести значения на экран

---
+++ src/main/java/com/github/demchenkovv/level13/lecture05/task1/Season.java
@@ -0,0 +1,8 @@
+package com.github.demchenkovv.level13.lecture05.task1;
+
+public enum Season {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+}
---
+++ src/main/java/com/github/demchenkovv/level13/lecture05/task1/Solution.java
@@ -0,0 +1,27 @@
+package com.github.demchenkovv.level13.lecture05.task1;
+
+/*
+Четыре сезона
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(Season.WINTER);
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+    }
+}
+
+/*
+
+Нужно создать набор значений (enum) для времен года.
+Для этого нужно в отдельном файле создать enum Season и объявить в нем 4 значения.
+В методе Solution выведи все перечисления с новой строки.
+
+Требования:
+•	В отдельном файле создай публичный enum Season и добавь значения: WINTER, SPRING, SUMMER, AUTUMN.
+•	В классе Solution выведи в консоли каждый сезон с новой строки. Используй перечисления напрямую: например, Season.WINTER.
+ */
