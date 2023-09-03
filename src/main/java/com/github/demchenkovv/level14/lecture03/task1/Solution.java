dcdacd1550ca70bcf69d62af767c169b1ec00589
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-03 23:14:24 +0300

    level14.lecture03.task1: Exeption try-catch / Перехват исключений в методе main

---
+++ src/main/java/com/github/demchenkovv/level14/lecture03/task1/Solution.java
@@ -0,0 +1,38 @@
+package com.github.demchenkovv.level14.lecture03.task1;
+
+/*
+Проглатываем исключение
+*/
+
+public class Solution {
+
+    public static String errorMessage = "не повезло";
+
+    public static void main(String[] args) {
+        try {
+            generateLuckyNumber();
+        } catch (Exception ex) {
+            System.out.println(errorMessage);
+        }
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception();
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
+}
+
+/*
+Перехвати исключение в методе main, чтобы программа компилировалась.
+При возникновении исключения нужно вывести на экран строку errorMessage.
+
+Требования:
+•	В методе main нужно добавить блок try-catch.
+•	Метод main не должен бросать проверяемые исключения.
+•	Метод main должен вызывать метод generateLuckyNumber.
+•	Метод generateLuckyNumber изменять нельзя.
+•	При возникновении исключения в методе main нужно вывести на экран строку errorMessage.
+ */
---
+++ src/main/java/com/github/demchenkovv/level14/lecture03/task2/Solution.java
@@ -0,0 +1,2 @@
+package com.github.demchenkovv.level14.lecture03.task2;public class Solution {
+}
