8024c9fe12510cefddfc1e989fda38bbfa176d95
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-03 23:51:08 +0300

    level14.lecture03.task3: Exception DRY: Don’t Repeat Yourself / Множественный перехват исключений

---
+++ src/main/java/com/github/demchenkovv/level14/lecture03/task5/Solution.java
@@ -0,0 +1,29 @@
+package com.github.demchenkovv.level14.lecture03.task5;
+
+/*
+Группировка исключений
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+        } catch (IllegalArgumentException | IllegalStateException | InterruptedException e) {
+            System.out.println("Произошло исключение на букву I");
+        } catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+        }
+    }
+}
+
+/*
+Объедини блоки catch с одинаковым кодом.
+
+Требования:
+•	Количество блоков catch должно быть минимальным.
+ */
