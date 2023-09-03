de2d913d92f92dff2960b4ef1ea47797dc43795f
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-03 23:20:17 +0300

    level14.lecture03.task2: Exeption checked throws / Обработка checked исключений в сигнатуре метода без блока try-catch

--- src/main/java/com/github/demchenkovv/level14/lecture03/task2/Solution.java
+++ src/main/java/com/github/demchenkovv/level14/lecture03/task2/Solution.java
@@ -1,2 +1,30 @@
-package com.github.demchenkovv.level14.lecture03.task2;public class Solution {
+package com.github.demchenkovv.level14.lecture03.task2;
+
+/*
+Пропускаем сквозь себя
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        generateLuckyNumber();
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception("не повезло");
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
 }
+
+/*
+Сделай так, чтобы программа компилировалась, не используя try-catch.
+
+Требования:
+•	В методе main не должно быть блока try-catch.
+•	Метод main должен бросать исключение.
+•	Метод main должен вызывать метод generateLuckyNumber().
+•	Метод generateLuckyNumber изменять нельзя.
+ */
