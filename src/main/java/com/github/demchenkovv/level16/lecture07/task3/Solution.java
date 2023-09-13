56a5116ed709df20628b82ba2f5bc4521ac57f89
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-13 20:43:31 +0300

    level16.lecture07.task3: LocalDateTime.parse(String, DateTimeFormatter), DateTimeFormatter.ofPattern() / Задать шаблон, чтобы можно было распарсить заданную строку

---
+++ src/main/java/com/github/demchenkovv/level16/lecture07/task3/Solution.java
@@ -0,0 +1,30 @@
+package com.github.demchenkovv.level16.lecture07.task3;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/*
+Напиши шаблон
+*/
+
+public class Solution {
+
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss dd/MM/yyyy");
+    static String timeString = "13:30:45 23/02/2019";
+
+    public static void main(String[] args) {
+        LocalDateTime dateTime = LocalDateTime.parse(timeString, dateTimeFormatter);
+
+        System.out.println(dateTime);
+    }
+}
+
+/*
+Инициализируй статическую переменную dateTimeFormatter.
+Подумай, какой шаблон ей задать, чтобы можно было распарсить строку timeString.
+В методе main создай объект LocalDateTime, используя строку timeString и твой шаблон dateTimeFormatter.
+
+Требования:
+•	Переменная dateTimeFormatter должна быть инициализирована при объявлении.
+•	Метод main должен создать объект LocalDateTime, распарсив строку timeString с помощью шаблона dateTimeFormatter.
+ */
