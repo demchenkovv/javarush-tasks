25bba8886c95aada487cc45f760592be6e2ae38c
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-14 13:40:13 +0300

    level17.lecture01.task1: Переопределение методов / Переопределить метод в классе-наследнике

---
+++ src/main/java/com/github/demchenkovv/level17/lecture01/task1/Animal.java
@@ -0,0 +1,7 @@
+package com.github.demchenkovv.level17.lecture01.task1;
+
+public class Animal {
+    public void move() {
+        System.out.println("Я бегу!");
+    }
+}
---
+++ src/main/java/com/github/demchenkovv/level17/lecture01/task1/Bat.java
@@ -0,0 +1,11 @@
+package com.github.demchenkovv.level17.lecture01.task1;
+
+/*
+Правильное движение
+*/
+
+public class Bat extends Animal {
+    public void move(){
+        System.out.println("Я лечу!");
+    }
+}
