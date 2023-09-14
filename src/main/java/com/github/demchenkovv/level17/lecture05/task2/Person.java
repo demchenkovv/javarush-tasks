01a3351850e45318fbb7632ebf459702a35a86af
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-14 21:44:26 +0300

    level17.lecture05.task2: Абстрактные классы и методы (abstract) / Из абстрактного класса сделать обычный, из абстрактных геттеров и сеттеров написать логику согласно общепринятым подходам

---
+++ src/main/java/com/github/demchenkovv/level17/lecture05/task2/Person.java
@@ -0,0 +1,39 @@
+package com.github.demchenkovv.level17.lecture05.task2;
+
+/*
+От абстракции к реальности
+*/
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+/*
+Класс Person объявлен как абстрактный. Это значит, что мы не можем создавать его экземпляры.
+Сделай из данного класса обычный, допиши недостающую логику, которая отвечает общепринятым подходам к инкапсуляции:
+геттеры должны возвращать, а сеттеры — устанавливать значения соответствующим полям.
+
+Требования:
+•	Класс Person не должен быть абстрактным.
+•	Метод getName() должен возвращать значение поля name.
+•	Метод setName(String) должен устанавливать переданное значение полю name.
+•	Метод getAge() должен возвращать значение поля age.
+•	Метод setAge(int) должен устанавливать переданное значение полю age.
+ */
