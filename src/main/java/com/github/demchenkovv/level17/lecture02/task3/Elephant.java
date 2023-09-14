a0b0df439fa118bf8d53c87279ba16474c7e786f
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-14 19:07:15 +0300

    level17.lecture02.task3: Полиморфизм. Наследование / Использовать instanceOf для определения типа и вывода сообщения в консоль

---
+++ src/main/java/com/github/demchenkovv/level17/lecture02/task3/Elephant.java
@@ -0,0 +1,4 @@
+package com.github.demchenkovv.level17.lecture02.task3;
+
+public class Elephant extends Herbivore {
+}
---
+++ src/main/java/com/github/demchenkovv/level17/lecture02/task3/Herbivore.java
@@ -0,0 +1,4 @@
+package com.github.demchenkovv.level17.lecture02.task3;
+
+public class Herbivore extends Animal {
+}
