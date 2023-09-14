11394c4069d6487360cf4cdd337637eee8f7df4f
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-14 21:39:08 +0300

    level17.lecture03: Polymorphism / Добавить пример полиморфизма с описанием

---
+++ src/main/java/com/github/demchenkovv/level17/lecture03/polimorphismExample/Whale.java
@@ -0,0 +1,7 @@
+package com.github.demchenkovv.level17.lecture03.polimorphismExample;
+
+public class Whale extends Cow{
+    public void printName() {
+        System.out.println("Я — кит");
+    }
+}
