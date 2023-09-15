43254be5b4634e37858f6ea3848c9428b0425e9c
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-15 22:14:27 +0300

    level18.lecture01: Functional Interfaces / 1. Через обычный класс; 2. Через анонимный класс; 3. Чере лямбда-выражение; 4. Через передачу ссылки на метод.

---
+++ src/main/java/com/github/demchenkovv/level18/lecture01/methodReferencesExample/FunctionalInterfaceExample.java
@@ -0,0 +1,11 @@
+package com.github.demchenkovv.level18.lecture01.methodReferencesExample;
+
+/**
+ * <a href=https://javarush.com/quests/lectures/questsyntaxpro.level18.lecture01>Ссылка на лекцию и комментарии FutureDev</a>
+ * <p> В самом классе-интерфейсе FunctionalInterfaceExample метод interfaceMethod() не реализован.
+ * Мы его реализуем каждый раз, когда создаём объект класса, который реализует данный интерфейс.
+ * Несколько примеров-вариантов как можно это сделать (см. скриншоты)
+ */
+public interface FunctionalInterfaceExample {
+    void interfaceMethod();
+}
