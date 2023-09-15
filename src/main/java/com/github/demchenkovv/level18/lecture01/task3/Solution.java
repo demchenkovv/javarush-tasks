b06598360b1b63fb941a9f7c2e758c1eb80e3fb1
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-15 19:17:22 +0300

    level18.lecture01.task3: Method References / Реализовать метод print так, чтобы метод списка forEach() принимал ссылку на метод

---
+++ src/main/java/com/github/demchenkovv/level18/lecture01/task3/Solution.java
@@ -0,0 +1,34 @@
+package com.github.demchenkovv.level18.lecture01.task3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Знакомство со ссылками на методы
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+        Collections.addAll(strings, "Hello", "Amigo", "JavaRush", "CodeGym");
+
+        print(strings);
+    }
+
+    public static void print(ArrayList<String> strings) {
+        strings.forEach(System.out::println);
+    }
+}
+
+/*
+В классе Solution публичный метод print(ArrayList<String>) выводит в консоли все элементы списка по порядку.
+Сейчас метод реализован с использованием метода списка forEach(), который принимает лямбда-выражение.
+Необходимо переписать реализацию метода print(ArrayList<String>), чтобы метод списка forEach()
+вместо лямбда-выражения принимал ссылку на метод.
+
+Метод main() не принимает участие в тестировании.
+
+Требования:
+•	В методе print(ArrayList<String>) нужно использовать метод списка forEach(), принимающий ссылку на метод.
+•	В методе print(ArrayList<String>) нужно выводить в консоли все элементы полученного списка по порядку.
+ */
