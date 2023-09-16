e6ed8d0c39f5c111451021b060b3c04cc405e300
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-16 21:46:11 +0300

    level18.lecture04.task1: Stream: map(Function<? super T, ? extends R>) / Преобразовать поток данных одного типа в другой, используя метод map(Function)

---
+++ src/main/java/com/github/demchenkovv/level18/lecture04/task1/Account.java
@@ -0,0 +1,19 @@
+package com.github.demchenkovv.level18.lecture04.task1;
+
+public class Account {
+    private String name;
+    private String email;
+
+    public Account(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
