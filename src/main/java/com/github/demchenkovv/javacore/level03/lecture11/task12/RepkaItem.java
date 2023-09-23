6ddd28e8fead3bf23841212fe0f9c24e8b92cc8f
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-23 22:21:30 +0300

    javacore.l03.l11.t12: Репка / Реализовать метод, который должен выводить фразу ..., реализовать интерфейс в классе, исправить логическую ошибку.

---
+++ src/main/java/com/github/demchenkovv/javacore/level03/lecture11/task12/RepkaItem.java
@@ -0,0 +1,5 @@
+package com.github.demchenkovv.javacore.level03.lecture11.task12;
+
+public interface RepkaItem {
+    public String getNamePadezh();
+}
---
+++ src/main/java/com/github/demchenkovv/javacore/level03/lecture11/task12/RepkaStory.java
@@ -0,0 +1,15 @@
+package com.github.demchenkovv.javacore.level03.lecture11.task12;
+
+import java.util.List;
+
+public class RepkaStory {
+    static void tell(List<Person> items) {
+        Person first;
+        Person second;
+        for (int i = items.size() - 1; i > 0; i--) {
+            second = items.get(i - 1);
+            first = items.get(i);
+            first.pull(second);
+        }
+    }
+}
