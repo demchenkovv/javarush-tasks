1dca816a4194bba120e1be0cf9649bb6f43d28f6
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-09-26 22:10:38 +0300

    javacore.l04.l04: Задачи на приведение типов

---
+++ src/main/java/com/github/demchenkovv/javacore/level04/lecture04/task2/Solution.java
@@ -0,0 +1,97 @@
+package com.github.demchenkovv.javacore.level04.lecture04.task2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Коты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String somename = bufferedReader.readLine();
+//        while (!somename.isBlank()) { валидатор не понимает isBlank()
+        while (!somename.equals("")) {
+            Cat cat = CatFactory.getCatByKey(somename);
+            System.out.println(cat.toString());
+            somename = bufferedReader.readLine();
+        }
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat;
+            switch (key) {
+                case "vaska":
+                    cat = new MaleCat("Василий");
+                    break;
+                case "murka":
+                    cat = new FemaleCat("Мурочка");
+                    break;
+                case "kiska":
+                    cat = new FemaleCat("Кисюлька");
+                    break;
+                default:
+                    cat = new Cat(key);
+                    break;
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "Я уличный кот " + getName();
+        }
+    }
+
+    static class MaleCat extends Cat {
+        MaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - солидный кошак по имени " + getName();
+        }
+    }
+
+    static class FemaleCat extends Cat {
+        FemaleCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "Я - милая кошечка по имени " + getName();
+        }
+    }
+}
+
+
+/*
+Коты
+Давай напишем программу, которая будет давать имена всем котикам, выпускаемым на нашей котофабрике.
+Для этого нужно:
+Считывать строки (параметры) с консоли, пока пользователь не введет пустую строку (Enter).
+Для каждого параметра (имени кота):
+создать объект cat класса Cat, который равен коту из getCatByKey(String параметр).
+вывести на экран cat.toString().
+
+Requirements:
+1. Программа должна считывать данные с клавиатуры.
+2. Программа должна прекращать считывать данные после ввода пустой строки.
+3. Программа должна выводить на экран описание каждого кота (cat.toString).
+4. Программа должна создавать объект класса Cat для каждого введенного имени кота (строки, считанной с клавиатуры) c помощью метода getCatByKey.
+ */
---
+++ src/main/java/com/github/demchenkovv/javacore/level04/lecture04/task3/Solution.java
@@ -0,0 +1,60 @@
+package com.github.demchenkovv.javacore.level04.lecture04.task3;
+
+/*
+Пора покушать
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Food food = new Food();
+        Selectable selectable = new Food();
+
+        callFoodMethods(food);
+        callSelectableMethods(selectable);
+    }
+
+    public static void callFoodMethods(Food food) {
+        //тут добавьте вызов методов для переменной food
+        food.onSelect();
+        food.onEat();
+    }
+
+    public static void callSelectableMethods(Selectable selectable) {
+        //тут добавьте вызов методов для переменной selectable
+        selectable.onSelect();
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    static class Food implements Selectable {
+        public void onEat() {
+            System.out.println("The food was eaten");
+        }
+
+        @Override
+        public void onSelect() {
+            System.out.println("The food was selected");
+        }
+    }
+}
+
+
+/*
+Пора покушать
+Давай напишем программу, которая поможет тебе выбрать, что съесть на обед.
+Для этого нужно:
+Реализовать интерфейс Selectable в классе Food.
+Метод onSelect() должен выводить на экран фразу "The food was selected".
+Подумай, какие методы можно вызвать для переменной food, а какие для — selectable.
+В методе callFoodMethods вызови методы onSelect, onEat, если это возможно.
+В методе callSelectableMethods вызови методы onSelect, onEat, если это возможно.
+Не используй явное приведение типов.
+
+Requirements:
+1. Интерфейс Selectable должен быть реализован в классе Food.
+2. Метод onSelect() в классе Food должен выводить на экран фразу "The food was selected".
+3. В методе callFoodMethods должны вызываться методы объекта типа Food.
+4. В методе callSelectableMethods должны вызываться методы доступные у любого объекта реализующего интерфейс Selectable.
+ */
