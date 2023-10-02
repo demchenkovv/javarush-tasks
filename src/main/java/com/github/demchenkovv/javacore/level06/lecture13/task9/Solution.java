f905671e80580a0bac7afb85433f58fd3ce54685
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-10-02 17:44:27 +0300

    jc.l06.l13: Многопоточность. Runnable / Написать код по образцу

---
+++ src/main/java/com/github/demchenkovv/javacore/level06/lecture13/task9/Solution.java
@@ -0,0 +1,79 @@
+package com.github.demchenkovv.javacore.level06.lecture13.task9;
+
+/*
+Создание по образцу
+*/
+
+public class Solution {
+    public static int number = 5;
+
+    public static void main(String[] args) {
+        new Thread(new CountdownRunnable(), "Уменьшаем").start();
+        new Thread(new CountUpRunnable(), "Увеличиваем").start();
+    }
+
+    public static class CountUpRunnable implements Runnable {
+        private int countIndexUp = 1;
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    Thread.sleep(500);
+                    System.out.println(toString());
+                    if (countIndexUp == number) return;
+                    countIndexUp += 1;
+
+                    // Или так, но тогда countIndexUp инициализировать с 0
+//                    ++countIndexUp;
+//                    System.out.println(toString());
+//                    Thread.sleep(500);
+//                    if (countIndexUp == Solution.number) return;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexUp;
+        }
+    }
+
+
+    public static class CountdownRunnable implements Runnable {
+        private int countIndexDown = Solution.number;
+
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println(toString());
+                    countIndexDown -= 1;
+                    if (countIndexDown == 0) return;
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException e) {
+            }
+        }
+
+        public String toString() {
+            return Thread.currentThread().getName() + ": " + countIndexDown;
+        }
+    }
+}
+
+
+/*
+Создание по образцу
+Разберись, как работает программа.
+По образу и подобию CountdownRunnable создай нить CountUpRunnable, которая выводит значения в нормальном порядке - от 1 до number.
+
+
+Requirements:
+1. Класс CountUpRunnable должен реализовывать интерфейс Runnable.
+2. Класс CountUpRunnable должен иметь публичный метод run без параметров.
+3. Метод run класса CountUpRunnable должен работать примерно 2,5 секунды.
+4. Метод run класса CountUpRunnable должен каждые полсекунды выводить имя потока, двоеточие и значение счетчика от 1 до 5 (например: "Увеличиваем: 1").
+5. Метод run класса CountUpRunnable должен вызывать Thread.sleep(500).
+ */
