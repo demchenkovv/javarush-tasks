016dc297b5da7cfb28aaf3abf95d6a70a9ce94a4
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-11-19 19:48:02 +0300

    mt0406 / Local class: Создание интерфейсов, наследование интерфейсов, абстрактные методы и их реализация наследниками, получение имени класса с помощью рефлексии.

---
+++ src/main/java/com/github/demchenkovv/multithreading/level04/lecture06/task3/Item.java
@@ -0,0 +1,10 @@
+package com.github.demchenkovv.multithreading.level04.lecture06.task3;
+
+public interface Item {
+
+    int getId();
+
+    double getPrice();
+
+    String getTM();
+}
---
+++ src/main/java/com/github/demchenkovv/multithreading/level04/lecture06/task3/Jeans.java
@@ -0,0 +1,8 @@
+package com.github.demchenkovv.multithreading.level04.lecture06.task3;
+
+public interface Jeans extends Item {
+
+    int getLength();
+
+    int getSize();
+}
