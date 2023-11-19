1d322058e62e2b067129409352f3493ccad937f2
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-11-19 15:56:28 +0300

    mt0404 / Local classes: Внутри методов создать локальные классы - реализации интерфейсов.

---
+++ src/main/java/com/github/demchenkovv/multithreading/level04/lecture04/task1/HasHeight.java
@@ -0,0 +1,5 @@
+package com.github.demchenkovv.multithreading.level04.lecture04.task1;
+
+public interface HasHeight {
+    double getHeight();
+}
---
+++ src/main/java/com/github/demchenkovv/multithreading/level04/lecture04/task1/HasWidth.java
@@ -0,0 +1,5 @@
+package com.github.demchenkovv.multithreading.level04.lecture04.task1;
+
+public interface HasWidth {
+    double getWidth();
+}
