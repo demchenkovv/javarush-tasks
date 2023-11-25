ef34ad4f949b8534a871b8fabf0d8e96abae0800
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-11-25 23:00:50 +0300

    mt0505 / task2505 / Без дураков: Thread.UncaughtExceptionHandler, daemon

---
+++ src/main/java/com/github/demchenkovv/multithreading/level05/lecture05/task2505/Solution.java
@@ -0,0 +1,47 @@
+package com.github.demchenkovv.multithreading.level05.lecture05.task2505;
+
+/* 
+Без дураков
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        MyThread myThread = new Solution().new MyThread("super secret key");
+        myThread.start();
+    }
+
+    public class MyThread extends Thread {
+        private String secretKey;
+
+        public MyThread(String secretKey) {
+            this.secretKey = secretKey;
+            setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+            // изменить на false, иначе дочерний поток главный поток
+            // закончит свою работу раньше, чем дочерний успеет отработать
+            setDaemon(false);
+        }
+
+        @Override
+        public void run() {
+            throw new NullPointerException("it's an example");
+        }
+
+        private class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+            public MyUncaughtExceptionHandler() {
+            }
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                try {
+                    Thread.sleep(500);
+                    System.out.println(String.format("%s, %s, %s", secretKey, t.getName(), e.getMessage()));
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        }
+    }
+}
+
