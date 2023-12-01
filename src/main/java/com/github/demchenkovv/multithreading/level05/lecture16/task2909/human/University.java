87da0bcdd129620e07cc8472808a5f135237105c
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-01 14:45:55 +0300

    mt0516 / task2909 / Большая задача: Рефакторинг Java: Рефакторинг (6)

--- src/main/java/com/github/demchenkovv/multithreading/level05/lecture16/task2909/human/University.java
+++ src/main/java/com/github/demchenkovv/multithreading/level05/lecture16/task2909/human/University.java
@@ -1,7 +1,9 @@
 package com.github.demchenkovv.multithreading.level05.lecture16.task2909.human;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class University {
     private List<Student> students = new ArrayList<>();
@@ -37,17 +39,21 @@ public class University {
         this.age = age;
     }
 
-    public Student getStudentWithAverageGrade() {
-        //TODO:
-        return null;
+    public Student getStudentWithAverageGrade(double averageGrade) {
+        String studentNotFound = "University#getStudentWithAverageGrade: Студент с таким средним баллом не найден.";
+        return students.stream().filter(student -> student.getAverageGrade() == averageGrade).findFirst()
+                .orElseThrow(() -> new NoSuchElementException(studentNotFound));
     }
 
-    public Student getStudentWithMaxAverageGrade(double averageGrade) {
-        //TODO:
-        return null;
+    public Student getStudentWithMaxAverageGrade() {
+        return students.stream().max(Comparator.comparingDouble(Student::getAverageGrade)).get();
     }
 
-    public void getStudentWithMinAverageGradeAndExpel() {
-        //TODO:
+    public Student getStudentWithMinAverageGrade() {
+        return students.stream().min(Comparator.comparingDouble(Student::getAverageGrade)).get();
+    }
+
+    public void expel(Student student) {
+        students.remove(student);
     }
 }
