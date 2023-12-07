fd13dcdb4ed0218cc7c2d0c7ba4b8135105656f7
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-07 20:23:37 +0300

    mt0615 / task3008 / Чат(2)

--- src/main/java/com/github/demchenkovv/multithreading/level06/lecture15/task3008/ConsoleHelper.java
+++ src/main/java/com/github/demchenkovv/multithreading/level06/lecture15/task3008/ConsoleHelper.java
@@ -1,4 +1,39 @@
 package com.github.demchenkovv.multithreading.level06.lecture15.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String message;
+        while (true) {
+            try {
+                message = bufferedReader.readLine();
+                break;
+            } catch (IOException ex) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return message;
+    }
+
+    public static int readInt() {
+        int number;
+        while (true) {
+            try {
+                number = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return number;
+    }
 }
