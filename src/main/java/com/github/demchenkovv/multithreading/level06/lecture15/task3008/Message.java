37367f6dc6d088ae4bc93467668fdb2aae2b4132
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-07 13:12:17 +0300

    mt0615 / task3008 / Чат(1)

---
+++ src/main/java/com/github/demchenkovv/multithreading/level06/lecture15/task3008/Message.java
@@ -0,0 +1,4 @@
+package com.github.demchenkovv.multithreading.level06.lecture15.task3008;
+
+public class Message {
+}
---
+++ src/main/java/com/github/demchenkovv/multithreading/level06/lecture15/task3008/Server.java
@@ -0,0 +1,8 @@
+package com.github.demchenkovv.multithreading.level06.lecture15.task3008;
+
+public class Server {
+
+    public static void main(String[] args) {
+
+    }
+}
