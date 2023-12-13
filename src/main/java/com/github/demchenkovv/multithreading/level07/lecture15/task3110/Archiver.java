5b3d3293df950843e9abe8790c2cf6dbc2cdd998
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-13 21:14:58 +0300

    mt0715 / task3110 / Archiver (8)

--- src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/Archiver.java
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/Archiver.java
@@ -1,39 +1,58 @@
 package com.github.demchenkovv.multithreading.level07.lecture15.task3110;
 
-import com.github.demchenkovv.multithreading.level07.lecture15.task3110.command.ExitCommand;
+import com.github.demchenkovv.multithreading.level07.lecture15.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.IOException;
 
 /**
  * Главный класс приложения "Архиватор"
  */
 public class Archiver {
 
-    public static void main(String[] args) throws Exception {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+    public static void main(String[] args) throws IOException {
+//        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+//
+//            System.out.println("Введите полный путь файла архива:");
+//            Path zipFile = Paths.get(reader.readLine()); // файл, в котором мы будем хранить сжатые данные
+//            while (!zipFile.isAbsolute()) {
+//                System.out.println("Введен некорректный путь. Убедитесь, что путь до файла является абсолютным.");
+//                zipFile = Paths.get(reader.readLine());
+//            }
+//            ZipFileManager zipFileManager = new ZipFileManager(zipFile);
+//
+//            System.out.println("Введите полное имя файла для архивации:");
+//            Path source = Paths.get(reader.readLine()); // файл, который мы будем сжимать
+//            while (!source.isAbsolute()) {
+//                System.out.println("Введен некорректный путь. Убедитесь, что путь до файла является абсолютным.");
+//                source = Paths.get(reader.readLine());
+//            }
+//
+//            zipFileManager.createZip(source);
+//        }
 
-            System.out.println("Введите полный путь файла архива:");
-            Path zipFile = Paths.get(reader.readLine()); // файл, в котором мы будем хранить сжатые данные
-            while (!zipFile.isAbsolute()) {
-                System.out.println("Введен некорректный путь. Убедитесь, что путь до файла является абсолютным.");
-                zipFile = Paths.get(reader.readLine());
+        Operation operation = null;
+        while (operation != Operation.EXIT) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException wrongEx) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception ex) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
-            ZipFileManager zipFileManager = new ZipFileManager(zipFile);
-
-            System.out.println("Введите полное имя файла для архивации:");
-            Path source = Paths.get(reader.readLine()); // файл, который мы будем сжимать
-            while (!source.isAbsolute()) {
-                System.out.println("Введен некорректный путь. Убедитесь, что путь до файла является абсолютным.");
-                source = Paths.get(reader.readLine());
-            }
-
-            zipFileManager.createZip(source);
         }
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+    }
+
+    /**
+     * Выводит в консоль список доступных команд и просит выбрать одну из них.
+     */
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        for (Operation operation : Operation.values()) {
+            ConsoleHelper.writeMessage(operation.ordinal() + " - " + operation.getTitle());
+        }
+        int numOperation = ConsoleHelper.readInt();
+        return Operation.values()[numOperation];
     }
 }
--- src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/Operation.java
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/Operation.java
@@ -4,10 +4,20 @@ package com.github.demchenkovv.multithreading.level07.lecture15.task3110;
  * Все команды, которые поддерживает наш архиватор
  */
 public enum Operation {
-    CREATE,
-    ADD,
-    REMOVE,
-    EXTRACT,
-    CONTENT,
-    EXIT;
+    CREATE("упаковать файлы в архив"),
+    ADD("добавить файл в архив"),
+    REMOVE("удалить файл из архив"),
+    EXTRACT("распаковать архив"),
+    CONTENT("просмотреть содержимое архива"),
+    EXIT("выход");
+
+    private String title;
+
+    Operation(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
