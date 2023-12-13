e7de5c389d1652c02b87fe5a417274f609934bb2
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-13 19:04:48 +0300

    mt0715 / task3110 / Archiver (7): exception

---
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/exception/PathIsNotFoundException.java
@@ -0,0 +1,9 @@
+package com.github.demchenkovv.multithreading.level07.lecture15.task3110.exception;
+
+/**
+ * Исключение PathIsNotFoundException будем кидать, если не сможем
+ * найти путь, в который нужно распаковать архив, или путь к файлу,
+ * который хотим запаковать, или любой другой путь.
+ */
+public class PathIsNotFoundException extends Exception {
+}
---
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/exception/WrongZipFileException.java
@@ -0,0 +1,8 @@
+package com.github.demchenkovv.multithreading.level07.lecture15.task3110.exception;
+
+/**
+ * Исключение WrongZipFileException будем кидать, если будет попытка
+ * сделать что-нибудь с архивом, который не существует.
+ */
+public class WrongZipFileException extends Exception {
+}
