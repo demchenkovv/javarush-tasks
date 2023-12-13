db59f27fd16d973707b0b7955f34cd51ea55d85f
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-13 18:20:38 +0300

    mt0715 / task3110 / Archiver (3): BufferedReader

---
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/Operation.java
@@ -0,0 +1,13 @@
+package com.github.demchenkovv.multithreading.level07.lecture15.task3110;
+
+/**
+ * Все команды, которые поддерживает наш архиватор
+ */
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT;
+}
