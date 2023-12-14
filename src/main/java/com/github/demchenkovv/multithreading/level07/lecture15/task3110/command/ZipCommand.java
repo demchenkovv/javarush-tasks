4e8c986504959cb6610b8a4b826a369f312967c3
Vitaliy Demchenko <demchenko.vv@gmail.com>
2023-12-14 11:26:43 +0300

    mt0715 / task3110 / Archiver (11)

--- src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/command/ZipCommand.java
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/command/ZipCommand.java
@@ -1,5 +1,16 @@
 package com.github.demchenkovv.multithreading.level07.lecture15.task3110.command;
 
+import com.github.demchenkovv.multithreading.level07.lecture15.task3110.ConsoleHelper;
+import com.github.demchenkovv.multithreading.level07.lecture15.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
 
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива:");
+        Path archivePath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(archivePath);
+    }
 }
--- src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/command/ZipCreateCommand.java
+++ src/main/java/com/github/demchenkovv/multithreading/level07/lecture15/task3110/command/ZipCreateCommand.java
@@ -1,9 +1,27 @@
 package com.github.demchenkovv.multithreading.level07.lecture15.task3110.command;
 
+import com.github.demchenkovv.multithreading.level07.lecture15.task3110.ConsoleHelper;
+import com.github.demchenkovv.multithreading.level07.lecture15.task3110.ZipFileManager;
+import com.github.demchenkovv.multithreading.level07.lecture15.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand {
 
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
 
+            ConsoleHelper.writeMessage("Введите полное имя файла или директорию для архивации:");
+            Path filePath = Paths.get(ConsoleHelper.readString());
+
+            zipFileManager.createZip(filePath);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException pathIsNotFoundException) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
