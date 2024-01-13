04a0c648c8bb20a7fbd7aa300dd0273f61889194
Vitaliy Demchenko <demchenko.vv@gmail.com>
2024-01-13 16:00:15 +0300

    cl0210 / task3207 / К серверу по RMI: java.rmi.Remote; java.rmi.RemoteException; java.rmi.registry.LocateRegistry; java.rmi.registry.Registry; java.rmi.server.UnicastRemoteObject;

---
+++ src/main/java/com/github/demchenkovv/collections/level02/lecture10/task3207/DoubleStringImpl.java
@@ -0,0 +1,9 @@
+package com.github.demchenkovv.collections.level02.lecture10.task3207;
+
+import java.rmi.RemoteException;
+
+public class DoubleStringImpl implements DoubleString {
+    public String doubleString(String str) throws RemoteException {
+        return str + str;
+    }
+}
