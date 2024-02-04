45e12efdc0134107a3d01e415fb38e0fb8363938
Vitaliy Demchenko <demchenko.vv@gmail.com>
2024-02-04 21:11:06 +0300

    cl0604 / task3609 / Рефакторинг MVC

---
+++ src/main/java/com/github/demchenkovv/collections/level06/lecture04/task3609/SpeedometerView.java
@@ -0,0 +1,8 @@
+package com.github.demchenkovv.collections.level06.lecture04.task3609;
+
+public class SpeedometerView {
+
+    public void printCarDetails(String brand, String model, int speed) {
+        System.out.println("Car: \nBrand: " + brand + "\nModel: " + model + "\nCurrent Speed: " + speed + "\n");
+    }
+}
